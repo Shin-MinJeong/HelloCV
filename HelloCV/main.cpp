30c04765d1995280dd582306b1c2d23a3165d642
Shin-MinJeong <sanman1115@gmail.com>
2024-02-27 16:45:39 +0900

    트랙바 실습

--- HelloCV/main.cpp
+++ HelloCV/main.cpp
@@ -9,51 +9,37 @@ Point pt;
 
 //이전 마우스 이벤트 발생 위치를 저장하는 함수
 void on_mouse(int event, int x, int y, int flags, void*);
+//트랙바 콜백 함수
+void on_level_change(int pos, void* userdata);
 
 int main() {
 
 	cout << "Hello OpenCV" << CV_VERSION << endl;
 
-	img = imread("lenna.bmp");  // 영상 읽기
+	//픽셀값이 0으로 초기화 된 400*400 크기의 영상 img
+	img = Mat::zeros(400, 400, CV_8UC1);
 
-	if (img.empty()) {				// 영상이 불러와지지 않는경우 empty = true
-		cout << "Image load failed";
-		return -1;
-	}
+	namedWindow("image");
+	
+	//트랙바 생성
+	//createTrackbar(트랙바이름, 트랙바 생성창, 트랙바 위치를 받을 주소, 트랙바 최대 위치, 콜백함수, 사용자 포인터)
+	createTrackbar("level", "image", 0, 10, on_level_change, (void*)&img);
 
-	//setMouseCallback() 함수를 사용하기 전에 마우스 이벤트를 받을 창 미리 생성
-	namedWindow("img");
-	setMouseCallback("img", on_mouse);
-
-	imshow("img", img);
+	imshow("image", img);
 	waitKey(0);
 
 	return 0;
 }
-	
-//마우스 콜백 함수 on_mouse() 함수에서 함께 사용할 영상을 전역 변수 img로 선언하여 사용
-//on_mouse() 함수에서는 sitch~case 구문을 이용하여 마우스 이벤트를 처리한다
-void on_mouse(int event, int x, int y, int flags, void*) {
-	switch (event) {
-	case EVENT_LBUTTONDOWN:
-		pt = Point(x, y);
-		cout << "EVENT_LBUTTONDOWN: " << x << ", " << y << endl;
-		break;
 
-	case EVENT_LBUTTONUP:
-		cout << "EVENT_LBUTTONUP: " << x << ", " << y << endl;
-		break;
+//트랙바 콜백 함수
+//트랙바 콜백 함수는 정해진 형식을 따라야 한다.
+//트랙바 콜백 함수(트랙바의 위치 정보, 사용자 데이터 포인터 값)
+void on_level_change(int pos, void* userdata)
+{
+    Mat img = *(Mat*)userdata;
 
-	case EVENT_MOUSEMOVE:
-		if (flags & EVENT_FLAG_LBUTTON) {
-			line(img, pt, Point(x, y), Scalar(0, 255, 255), 2);
-			imshow("img", img);
-			pt = Point(x, y);
-		}
-		break;
-	default:
-		break;
-		}
-	}
+        img.setTo(pos * 16); //픽셀값 = 사용자데이터*16
+    imshow("image", img);
+}
 
 
