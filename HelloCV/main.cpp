c0271cd2db990dc571f0dd775d61c6d1bd0d5ab4
Shin-MinJeong <sanman1115@gmail.com>
2024-03-04 16:05:04 +0900

    산술연산 실습

--- HelloCV/main.cpp
+++ HelloCV/main.cpp
@@ -4,62 +4,46 @@
 using namespace cv;
 using namespace std;
 
-//마스크 함수
-void mask_setTo();
-void mask_copyTo();
 
 int main(void) {
 
-	cout << "Hello OpenCV" << CV_VERSION << endl;
 
-	mask_setTo();
-	mask_copyTo();
+	Mat src1 = imread("lenna256.bmp", IMREAD_GRAYSCALE);
+	Mat src2 = imread("square.bmp",   IMREAD_GRAYSCALE);
+
+	if (src1.empty() || src2.empty()) {
+		cerr << "이미지 로드 실패" << endl;
+		return -1;
+	}
+
+	imshow("src1", src1);
+	imshow("src2", src2);
+
+	Mat dst1, dst2, dst3, dst4 ;
+
+	//덧셈연산 
+	//add(입력1, 입력2, 출력, mask, 행렬의 깊이)
+	add(src1, src2, dst1);
+	//뺄셈연산 
+	//subtract(입력1, 입력2, 출력,mask, 행렬의 깊이)
+	subtract(src1, src2, dst2);
+	//평균연산 
+	//addWeighted(입력1, 가중치1, 입력2, 가중치2, 추가값, 출력, 행렬의 깊이)
+	addWeighted(src1, 0.5, src2, 0.5, 0, dst3);
+	//차이연산
+	//absdiff(입력1, 입력2, 출력)
+	absdiff(src1, src2, dst4);
+
+	imshow("dst1", dst1);
+	imshow("dst2", dst2);
+	imshow("dst3", dst3);
+	imshow("dst4", dst4);
+	imshow("dst1", dst1);
+
+	waitKey();
 
 	return 0;
 }
 
 
-void mask_setTo() {
 
-	Mat src = imread("lenna.bmp", IMREAD_COLOR);
-	Mat mask = imread("mask_smile.bmp", IMREAD_GRAYSCALE);
-
-	if (src.empty() || mask.empty()) {
-		cerr << "Image load failed!" << endl;
-		return;
-	}
-
-	//setTo(행렬 원소 값, 마스크 행렬)
-	//src 이미지를 마스크 값 만큼 노란색으로 표시한다.
-	//Scalar(파란색, 녹색, 빨간색, 투명도)
-	src.setTo(Scalar(0, 255, 255), mask);
-
-	imshow("src", src);
-	imshow("mask", mask);
-
-	waitKey();
-	destroyAllWindows();
-}
-
-void mask_copyTo() {
-	Mat src = imread("airplane.bmp", IMREAD_COLOR);
-	Mat mask = imread("mask_plane.bmp", IMREAD_GRAYSCALE);
-	Mat dst = imread("field.bmp", IMREAD_COLOR);
-
-	if (src.empty() || mask.empty() || dst.empty()) {
-		cerr << "Image load failed" << endl;
-		return;
-	}
-
-	//copyTo(복사본 행렬, 마스크 행렬)
-	//mask 영상의 픽셀값이 0이 아닌 위치에서만 복사한다.(검정색이 아닌 것)
-	//mask 변수에 0이 아닌 위치 값을 dst로 복사한다.
-	src.copyTo(dst, mask);
-
-	imshow("src", src);
-	imshow("mask", mask);
-	imshow("dst", dst);
-
-	waitKey(0);
-	destroyAllWindows();
-}
